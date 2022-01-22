b68fcfff01bc52fb48095dd55b4be03e8920557b
sanskar chaudhary <sanskarc55@gmail.com>
2022-01-22 14:37:28 +0545

    Initial commit

---
+++ app/src/main/java/com/example/applet/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.applet.Prevalent;
+
+
+import com.example.applet.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
